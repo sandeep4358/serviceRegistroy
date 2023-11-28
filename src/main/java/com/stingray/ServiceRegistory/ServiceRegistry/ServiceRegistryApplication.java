3385b8ca36f7ab3e86ebc06ac3fca08236ed3630
sandeep4358 <sandeep4358@gmail.com>
2023-11-29 01:33:27 +0530

    first commit

---
+++ src/main/java/com/stingray/ServiceRegistory/ServiceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,20 @@
+package com.stingray.ServiceRegistory.ServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServiceRegistryApplication.class);
+	}
+}
